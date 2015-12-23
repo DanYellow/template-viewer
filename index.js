debaccb52e310b937a69fa0e6acf616a7ffd32b9
Dan42K <jeanlouis.danielo@sfr.fr>
2015-12-23 23:12:31 +0100

    improvements and fix issues in index.js

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ require('events').EventEmitter.defaultMaxListeners = Infinity;
 // @desc : Create an array of object to create printscreens
 // @param {Object} options - Options of the module
 //  options.tplToRender {Array}: Array of html file to rendered. Default : empty. NOTE : Avoid a big number of file or else your computer can be very slow
-//  options.rootDir {String}: Root directory for printscreens. **PRINTSCREENS** ARE ALWAYS PUT IN "printscreens" folder - Default : ".public/". NOTE : if the directory doesn't exist it will be created
+//  options.dist {String}: Root directory for printscreens. **PRINTSCREENS** ARE ALWAYS PUT IN "printscreens" folder - Default : ".public/". NOTE : if the directory doesn't exist it will be created
+//  options.src {String}: Directory of source files | Default './dev'
 //  option.port {Number}: Server port. | Default 3000
 //  option.optimization {Boolean}: Precise if you want to optimize the printscreen rendering. When it sets to "YES", ONLY new pages are rendered, the old one keep their old image - Default : true
+//  option.tplExtension {String}: Extension of your source file | Default ".html"
 
 // @returns {Array} : Array of objects 
 var PrintscreensDatas = function PrintscreensDatas(options) {
@@ -22,38 +24,57 @@ var PrintscreensDatas = function PrintscreensDatas(options) {
   var filesCountMax = 0; // Number of element eligible for printscreens
   var filesCount = 0;
   var opts;
+  var newFile;
   
   this.init = function(options) {
 
     opts = assign({}, options);
     if (!opts.tplToRender) opts.tplToRender = [];
-    if (!opts.rootDir) opts.rootDir = './public';
+    if (!opts.dist) opts.dist = './public';
+    if (!opts.src) opts.src = './dev';
     if (!opts.port) opts.port = 3000;
+    if (!opts.tplExtension) opts.tplExtension = '.html';
     if (_.isUndefined(opts.optimization)) opts.optimization = true;
     opts.imgExtension = 'jpg';
 
-    var printscreensDestDirectory = opts.rootDir + '/printscreens';
+    var printscreensDestDirectory = opts.dist + '/printscreens';
     this.files = [];
-    if (!fs.existsSync(opts.rootDir)) {
-      fs.mkdirSync(opts.rootDir);
+    if (!fs.existsSync(opts.dist)) {
+      fs.mkdirSync(opts.dist);
+    };
+
+    if (!fs.existsSync(printscreensDestDirectory)) {
+      fs.mkdirSync(printscreensDestDirectory);
+    };
+
+    if (!fs.existsSync(opts.src)) {
+      console.log("This directory doesn't exists !");
+      return;
     };
 
     var generatePrintscreens = function generatePrintscreens () {
-      this.files = fs.readdirSync(opts.rootDir);
+      this.files = fs.readdirSync(opts.src);
+
       this.files = this.files.filter(function(file) {
         // We want only "html" files and files wanted
-        return file.substr(-5) === '.html' && opts.tplToRender.indexOf(file.split('.html')[0]) > -1;
+        return file.substr(-5) === opts.tplExtension && opts.tplToRender.indexOf(file.split(opts.tplExtension)[0]) > -1;
       }).map(function(file) {
         // Prefix every html file by the root for pageres
         return 'http://127.0.0.1:' + opts.port + '/' + file;
       });
-
-      filesCountMax = this.files.length;
-
+      
       var imgInTheDirectory = fs.readdirSync(printscreensDestDirectory);
       var futureFilesInTheDirectory = this.files.map(function(file) { return slugify(file) + '.' + opts.imgExtension });
+      var newPagesURL = _.difference(futureFilesInTheDirectory, imgInTheDirectory).map(function(file) {
+        newFile = file.replace('.' + opts.imgExtension, '');
+        newFile = newFile.split('!')[newFile.split('!').length - 1];
+        return 'http://127.0.0.1:' + opts.port + '/' + newFile;
+      });
+
+      filesCountMax = opts.optimization === true ? newPagesURL.length : this.files.length;
+      
       // If user want optimization we render only image of new pages
-      var pagesURL = opts.optimization === true ? _.difference(futureFilesInTheDirectory, imgInTheDirectory) : this.files;
+      var pagesURL = opts.optimization === true ? newPagesURL : this.files;
       
       pageRendering(pagesURL, printscreensDestDirectory);
     }
@@ -86,7 +107,6 @@ var PrintscreensDatas = function PrintscreensDatas(options) {
         console.log("issue with image parsing");
       }
 
-      
 
       return {templates: printscreensArray};
     };
@@ -94,6 +114,7 @@ var PrintscreensDatas = function PrintscreensDatas(options) {
     function pageRendering (array, directory) {
       array.forEach(function(file) {
         // Prefix every html file by the root
+
           var pageres = new Pageres()
             .src(file, ['1000x1000'], {
               crop: false,
